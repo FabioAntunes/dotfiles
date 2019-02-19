96917eef4120fd738d120f6af7fa0867224eb2e6
Fabio Antunes <fabioantuness@gmail.com>
2019-02-19 10:54:18 +0000

    feat: aws-okta completions

--- fish/completions/aws-okta.fish
+++ fish/completions/aws-okta.fish
@@ -1,2 +1,21 @@
-complete -c aws-okta --condition '__fish_seen_subcommand_from exec okta-staging okta-dev okta-prod' --argument 'aws' --wraps aws
-complete -c aws-okta --condition '__fish_seen_subcommand_from exec okta-staging okta-dev okta-prod' --argument 'terraform' --wraps terraform
+function __fish_okta_complete_profiles
+  cat ~/.aws/config | grep -e "\[[^(okta)]" | awk -F'[] []' '{print $3}'
+end
+
+function __fish_complete_okta_subcommand
+  set -l tokens (commandline -opc) (commandline -ct)
+  set -l index (contains -i -- -- (commandline -opc))
+  set -e tokens[1..$index]
+  complete -C"$tokens"
+end
+
+set -l okta_commands add exec help login version
+complete -f -c aws-okta -n "not __fish_seen_subcommand_from $okta_commands" -a add -d 'add your okta credentials'
+complete -f -c aws-okta -n "not __fish_seen_subcommand_from $okta_commands" -a exec -d 'exec will run the command specified with aws credentials set in the environment'
+complete -f -c aws-okta -n "not __fish_seen_subcommand_from $okta_commands" -a help -d 'help about any command'
+complete -f -c aws-okta -n "not __fish_seen_subcommand_from $okta_commands" -a login -d 'login will authenticate you through okta and allow you to access your AWS environment through a browser'
+complete -f -c aws-okta -n "not __fish_seen_subcommand_from $okta_commands" -a version -d 'print version'
+complete -f -c aws-okta -n "__fish_seen_subcommand_from exec; and not __fish_seen_subcommand_from (__fish_okta_complete_profiles)" -a "(__fish_okta_complete_profiles)"
+complete -f -c aws-okta -n "__fish_seen_subcommand_from (__fish_okta_complete_profiles); and not __fish_seen_subcommand_from --" -a "--"
+
+complete -c aws-okta -n "contains -- -- (commandline -opc)" -x -a "(__fish_complete_okta_subcommand)"
