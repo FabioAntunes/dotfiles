b468b029694d834e6872824305833bb5aed8a876
Fabio Antunes <fabioantuness@gmail.com>
2019-04-15 14:52:57 +0100

    Chore: updates okta completions

--- fish/completions/aws-okta.fish
+++ fish/completions/aws-okta.fish
@@ -9,13 +9,31 @@ function __fish_complete_okta_subcommand
   complete -C"$tokens"
 end
 
-set -l okta_commands add exec help login version
-complete -f -c aws-okta -n "not __fish_seen_subcommand_from $okta_commands" -a add -d 'add your okta credentials'
-complete -f -c aws-okta -n "not __fish_seen_subcommand_from $okta_commands" -a exec -d 'exec will run the command specified with aws credentials set in the environment'
-complete -f -c aws-okta -n "not __fish_seen_subcommand_from $okta_commands" -a help -d 'help about any command'
-complete -f -c aws-okta -n "not __fish_seen_subcommand_from $okta_commands" -a login -d 'login will authenticate you through okta and allow you to access your AWS environment through a browser'
-complete -f -c aws-okta -n "not __fish_seen_subcommand_from $okta_commands" -a version -d 'print version'
-complete -f -c aws-okta -n "__fish_seen_subcommand_from exec; and not __fish_seen_subcommand_from (__fish_okta_complete_profiles)" -a "(__fish_okta_complete_profiles)"
-complete -f -c aws-okta -n "__fish_seen_subcommand_from (__fish_okta_complete_profiles); and not __fish_seen_subcommand_from --" -a "--"
+function __fish_okta_needs_command
+    set cmd (commandline -opc)
+    if [ (count $cmd) -eq 1 ]
+        return 0
+    end
+    return 1
+end
 
-complete -c aws-okta -n "contains -- exec -- (commandline -opc)" -x -a "(__fish_complete_okta_subcommand)"
+function __fish_okta_using_command
+    set cmd (commandline -opc)
+    if [ (count $cmd) -gt 1 ]
+        if [ $argv[1] = $cmd[2] ]
+            return 0
+        end
+    end
+    return 1
+end
+
+set -l okta_commands add exec help login version
+complete -f -c aws-okta -n "__fish_okta_needs_command" -a add -d 'add your okta credentials'
+complete -f -c aws-okta -n "__fish_okta_needs_command" -a exec -d 'exec will run the command specified with aws credentials set in the environment'
+complete -f -c aws-okta -n "__fish_okta_needs_command" -a help -d 'help about any command'
+complete -f -c aws-okta -n "__fish_okta_needs_command" -a login -d 'login will authenticate you through okta and allow you to access your AWS environment through a browser'
+complete -f -c aws-okta -n "__fish_okta_needs_command" -a version -d 'print version'
+complete -f -c aws-okta -n "__fish_okta_using_command exec; and not __fish_seen_subcommand_from (__fish_okta_complete_profiles)" -a "(__fish_okta_complete_profiles)"
+complete -f -c aws-okta -n "__fish_okta_using_command exec; and __fish_seen_subcommand_from (__fish_okta_complete_profiles); and not __fish_seen_subcommand_from --" -a "--"
+
+complete -c aws-okta -n "__fish_okta_using_command exec; and __fish_seen_subcommand_from (__fish_okta_complete_profiles); and contains -- -- (commandline -opc)" -x -a "(__fish_complete_okta_subcommand)"
